6555e09667825d67a3927e75476e6352102456dc
Imanol Gomez <yo@imanolgomez.net>
2017-07-29 18:39:17 +0200

    Changed Arduino for communication protocol

--- Arduino/SerialColor/SerialColor.ino
+++ Arduino/SerialColor/SerialColor.ino
@@ -6,7 +6,7 @@
 #define CHIPSET     WS2811    //Choose your chipset
 #define NUM_LEDS    144         //No. of LEDs in your strip - 48led/m
 #define BRIGHTNESS  255        //Overall brightness of the LEDs on a scale from 0(off)-255(max birghtness)
-#define DATA_PIN 5
+#define DATA_PIN 2  
 #define DATA_CLK 4
 
 // Define the array of leds
@@ -15,7 +15,7 @@ CRGB    gColor =  CRGB::Black;
 
 void setup() { 
       
-    Serial.begin(9600);        // connect to the serial port
+    Serial.begin(115200);        // connect to the serial port
     
 
     FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip ); 
@@ -27,25 +27,57 @@ void setup() {
 
 void loop() { 
 
-  // if there's any serial available, read it:
-  while (Serial.available() > 0) {
+//  if(Serial.available()){
+//     byte header = Serial.read();
+//      if(header == 'c')
+//      {
+//         while (Serial.available() > 0) {
+//            int val = Serial.read();
+//         }
+//         fill_solid(leds, NUM_LEDS, CRGB::Red);
+//         FastLED.show();
+//         Serial.println("Color received");
+//      }
+//       else if(header == '?')
+//      {
+//           Serial.print("OK"); 
+//      }
+//  } 
 
-      byte red = Serial.read();
-      byte green = Serial.read();
-      byte blue = Serial.read();
+    // if there's any serial available, read it:
+    while (Serial.available() > 0) {
+     byte header = Serial.read();
+      if(header == 'c')
+      {
+          fill_solid(leds, NUM_LEDS, CRGB::Red);
+          FastLED.show(); 
+          byte red = Serial.read();
+          byte green = Serial.read();
+          byte blue = Serial.read();
 
-    // look for the newline. That's the end of your sentence:
-    if (Serial.read() == '\n') {
-      // constrain the values to 0 - 255 
+          // look for the newline. That's the end of your sentence:
+          if (Serial.read() == '\n') {
+            // constrain the values to 0 - 255 
+      
+             gColor.r = red;
+             gColor.g = green;
+             gColor.b = blue;
+      
+             fill_solid(leds, NUM_LEDS, gColor);
+             FastLED.show(); 
 
-       gColor.r = red;
-       gColor.g = green;
-       gColor.b = blue;
+//              while (Serial.available() > 0) {
+//                int val = Serial.read();
+//              }
 
-       fill_solid(leds, NUM_LEDS, gColor);
-       FastLED.show(); 
-    }
-  }
-
-
+             break;
+          }
+          }
+          else if(header == '?')
+          {
+               Serial.print("OK"); 
+               break;
+          }
+     
+     }
 }
