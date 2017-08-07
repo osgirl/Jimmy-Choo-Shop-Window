0b97ab2001653f3e6f58bd1ac88fcb2f73dea36c
ImanolGo <yo@imanolgomez.net>
2017-08-07 19:02:02 +0000

    Added Scripts

---
+++ Scripts/runAllways.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#-----------------------------------------------------------
+#
+# start Jimmy Choo Application
+#
+#-----------------------------------------------------------
+
+echo '-> Jimmy Choo App Script'
+
+dir=$(dirname $0)
+cd $dir
+
+echo '-> Starting Jimmy Choo Application'
+
+PROCESS=JimmyChooShopWi
+
+cd ../OpenFrameworks/JimmyChooShopWindowApp/bin/
+
+while true; do
+	if pgrep $PROCESS
+		then
+			echo '-> JimmyChooShopWi process already exit'
+		else
+			echo '-> Starting JimmyChooShopWi process...'
+			./JimmyChooShopWindowApp &
+	fi
+	sleep 30
+done
